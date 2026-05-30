ef06ce7e5dfefeffa3e3c858d5ec8d6e77771b41
Nenieco-001 <Nenieco.001@gmail.com>
2026-05-30 16:43:23 +0800

    feat: 实现 TransportData 数据层与文件持久化

---
+++ inc/data/transport_data.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+
+#include "model/transport_type.h"
+
+class TransportData {
+  private:
+    std::vector<City> cities_;  // 存储城市信息
+    std::vector<Trip> trips_;   // 存储班次信息
+  public:
+    // 构造函数
+    TransportData() = default;
+    // 城市添加
+    void addCity(const City& city);
+    // 城市删除
+    void removeCity(int city_id);
+    // 班次添加
+    void addTrip(const Trip& trip);
+    // 班次删除
+    void removeTrip(int trip_id);
+    // 获取出发时间窗口内的班次 - 按出发时间排序
+    std::vector<Trip> getDeparturesInWindow(int from_city_id, int from_time, int to_time, TransportType type) const;
+    // 获取指定时间之后的班次 - 按出发时间排序
+    std::vector<Trip> getDeparturesAfter(int from_city_id, int earliest_time, TransportType type) const;
+    // 获取所有城市列表
+    const std::vector<City>& getAllCities() const;
+    // 获取所有班次列表
+    const std::vector<Trip>& getAllTrips() const;
+};
